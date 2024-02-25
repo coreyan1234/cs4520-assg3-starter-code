b83eecf9e98c3d2a824ed4e7f2e20964a57a4423
coreyan1234 <coreyan1234@gmail.com>
2024-02-25 18:06:48 -0500

    Fixed the navigation path. App now correctly navigates when clicking on MVP and MVVM buttons

--- app/src/main/java/com/cs4520/assignment3/MvpPresenter.kt
+++ app/src/main/java/com/cs4520/assignment3/MvpPresenter.kt
@@ -8,7 +8,8 @@ class MvpPresenter(private val view: MvpContract.View): MvpContract.Presenter {
         val num2 = view.getNum2()
         if (model.isValidInput(num1, num2)) {
             val result = model.add(num1, num2)
-            view.showResult(result.toString())
+            val resultString = "Result: $result"
+            view.showResult(resultString)
             view.clearInputs()
         }
         else {
@@ -21,7 +22,8 @@ class MvpPresenter(private val view: MvpContract.View): MvpContract.Presenter {
         val num2 = view.getNum2()
         if (model.isValidInput(num1, num2)) {
             val result = model.subtract(num1, num2)
-            view.showResult(result.toString())
+            val resultString = "Result: $result"
+            view.showResult(resultString)
             view.clearInputs()
         }
         else {
@@ -34,7 +36,8 @@ class MvpPresenter(private val view: MvpContract.View): MvpContract.Presenter {
         val num2 = view.getNum2()
         if (model.isValidInput(num1, num2)) {
             val result = model.multiply(num1, num2)
-            view.showResult(result.toString())
+            val resultString = "Result: $result"
+            view.showResult(resultString)
             view.clearInputs()
         }
         else {
@@ -47,7 +50,8 @@ class MvpPresenter(private val view: MvpContract.View): MvpContract.Presenter {
         val num2 = view.getNum2()
         if (model.isValidInput(num1, num2) && num2 != 0.0) {
             val result = model.divide(num1, num2)
-            view.showResult(result.toString())
+            val resultString = "Result: $result"
+            view.showResult(resultString)
             view.clearInputs()
         }
         else {
--- app/src/main/java/com/cs4520/assignment3/fragments/HomeFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.cs4520.assignment3.R
 import com.cs4520.assignment3.databinding.FragmentHomeBinding
 
@@ -31,24 +32,12 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Button listeners
-        binding.buttonMvp.setOnClickListener{ navigateToMVPFragment() }
-        binding.buttonMvvm.setOnClickListener{ navigateToMVVMFragment() }
-    }
-
-    private fun navigateToMVPFragment() {
-        // Navigate to MVPFragment
-        val transaction = parentFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, MvpFragment())
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-
-    private fun navigateToMVVMFragment() {
-        // Navigate to MVVMFragment
-        val transaction = parentFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, MvvmFragment())
-        transaction.addToBackStack(null)
-        transaction.commit()
+        binding.buttonMvp.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_mvpFragment)
+        }
+        binding.buttonMvvm.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_mvvmFragment)
+        }
     }
 
     override fun onDestroyView() {
